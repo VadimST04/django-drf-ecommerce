f2825de814a996b14c72a071bb3872552e344800
Vadym Starchak <vadikstarchak04@gmail.com>
2023-08-29 18:10:23 +0200

    add SwaggerUI

--- drfecommerce/settings.py
+++ drfecommerce/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
 
     'product',
 ]
@@ -114,3 +115,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Ecommerce',
+}
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from product.models import Category, Brand, Product
 from product.serializers import CategorySerializer, BrandSerializer, ProductSerializer
@@ -12,6 +13,7 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
